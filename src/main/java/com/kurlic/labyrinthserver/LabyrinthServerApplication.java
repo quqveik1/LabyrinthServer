65b5db33a08ae159e4215a46f297d52bc8099f6b
Alex Bogomol <quqveik.1@gmail.com>
2023-06-08 22:57:42 +0300

    init commit

---
+++ src/main/java/com/kurlic/labyrinthserver/LabyrinthServerApplication.java
@@ -0,0 +1,13 @@
+package com.kurlic.labyrinthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabyrinthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabyrinthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kurlic/labyrinthserver/LabyrinthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurlic.labyrinthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabyrinthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
