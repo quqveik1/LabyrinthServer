bcbdbc3c93a812e2f56015ce362fda3d6ae42f9a
Alex Bogomol <quqveik.1@gmail.com>
2023-06-09 12:52:47 +0300

    web port

--- src/main/java/com/kurlic/labyrinthserver/TestController.java
+++ src/main/java/com/kurlic/labyrinthserver/TestController.java
@@ -1,4 +1,7 @@
+package com.kurlic.labyrinthserver;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
